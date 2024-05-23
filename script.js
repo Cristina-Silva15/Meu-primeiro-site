656087956e55fea5928481c5785974f15a18252e
Cristina <cristinasilvaa20@gmail.com>
2024-05-23 19:04:30 -0300

    formulario atualizado

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.btn');
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var targetId = this.getAttribute('data-target');
-            var target = document.getElementById(targetId);
-            if (target.style.display === 'none' || target.style.display === '') {
-                target.style.display = 'block';
-            } else {
-                target.style.display = 'none';
+    var radioButtons = document.querySelectorAll('input[name="tipo"]');
+
+    radioButtons.forEach(function(radioButton) {
+        radioButton.addEventListener('change', function() {
+            var dicaFields = document.getElementById('dicaFields');
+            var receitaFields = document.getElementById('receitaFields');
+
+            if (this.value === 'dica') {
+                dicaFields.style.display = 'block';
+                receitaFields.style.display = 'none';
+            } else if (this.value === 'receita') {
+                dicaFields.style.display = 'none';
+                receitaFields.style.display = 'block';
             }
         });
     });
