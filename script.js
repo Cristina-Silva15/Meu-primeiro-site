caae1f09a040b27d4c0a639102bc311f6680ae9b
Cristina <cristinasilvaa20@gmail.com>
2024-05-24 16:28:57 -0300

    criado botões retrateis, bordas, mudança na cor de alguns itens

--- script.js
+++ script.js
@@ -34,3 +34,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+
+//interação dos botoes retrateis das dicas
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.dicas');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var targetId = this.getAttribute('data-target');
+            var target = document.getElementById(targetId);
+            if (target.style.display === 'none' || target.style.display === '') {
+                target.style.display = 'block';
+            } else {
+                target.style.display = 'none';
+            }
+        });
+    });
+});
