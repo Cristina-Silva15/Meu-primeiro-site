b3044813d9b5be4ede3e1271c70ee8ec3a82c27b
Cristina <cristinasilvaa20@gmail.com>
2024-05-23 21:38:05 -0300

    alinhamento

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//interação dos inputs do formulario
 document.addEventListener('DOMContentLoaded', function() {
     var radioButtons = document.querySelectorAll('input[name="tipo"]');
 
@@ -12,6 +13,23 @@ document.addEventListener('DOMContentLoaded', function() {
             } else if (this.value === 'receita') {
                 dicaFields.style.display = 'none';
                 receitaFields.style.display = 'block';
+            }
+        });
+    });
+});
+
+
+//interação dos botoes retrateis das receitas 
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.btn');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var targetId = this.getAttribute('data-target');
+            var target = document.getElementById(targetId);
+            if (target.style.display === 'none' || target.style.display === '') {
+                target.style.display = 'block';
+            } else {
+                target.style.display = 'none';
             }
         });
     });
